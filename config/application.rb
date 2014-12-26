685cf453388150a6795526cf16534a45bbfdba92
Ahmet <ahmet@Ahmet-MacBook-Pro.local>
2014-12-26 20:29:19 +0200

    make it better

--- config/application.rb
+++ config/application.rb
@@ -20,11 +20,8 @@ module HayriEsmer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-
-    config.assets.precompile += ["fontawesome-webfont.ttf",
-                                 "fontawesome-webfont.eot",
-                                 "fontawesome-webfont.svg",
-                                 "fontawesome-webfont.woff"]
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
   end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -75,9 +75,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  #config.assets.initialize_on_precompile=false
-
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  config.assets.precompile += %w(.svg .eot .woff .ttf)
 end
