b378b37ea168de304318e189037b9c6c0f92f5b3
Ahmet <ahmet@Ahmet-MacBook-Pro.local>
2015-01-10 22:51:07 +0200

    make it better

--- app/controllers/social_media_controller.rb
+++ app/controllers/social_media_controller.rb
@@ -14,25 +14,20 @@ class SocialMediaController < ApplicationController
         config.access_token        = "332183668-lIbxi4pZoX5oRD4UgwDM4fGHF1EclFSYJlUHXKeU"
         config.access_token_secret = "Al6TbJtVWJoVpfWDxHXuU11KL8wApmPj64XX5dM53Wn7g"
       end
-
-      @twitter = @@client.user_timeline(count: 3)
+      @twitter = @@client.user_timeline({ :count => 3, :include_rts => false })
       Rails.cache.write('twitter', @twitter, expires_in: 30.minute)
 
+      puts 'Twitter canlı data'
     end
-
     render json: @twitter
   end
 
 
 
   def instagram
-
     @instagram = Rails.cache.read('instagram')
-
     if @instagram.nil?
-
       @instagram = Instagram.user_recent_media("1080343853", {:count => 9})
-
       Rails.cache.write('instagram', @instagram, expires_in: 30.minute)
       puts 'yok'
     end
